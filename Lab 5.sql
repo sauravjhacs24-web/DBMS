96b03f80fbe94d2b23644e24a44de349bae7f1db
sauravjhacs24-web <sauravjha.cs24@bmsce.ac.in>
2025-11-12 23:45:35 +0530

    Add files via upload

---
+++ Lab 5.sql
@@ -0,0 +1,84 @@
+create database if not exists employee_database;
+show databases;
+use employee_database;
+create table dept(deptno decimal(2,0) primary key, dname varchar(14) default NULL,loc varchar(13) default NULL);
+create table emp(empno decimal(4,0) primary key, ename varchar(14) default NULL, mgr_no decimal(4,0) default null,hiredate date  default NULL,sal decimal(7,2) default null, deptno decimal(2,0) references dept(deptno) on delete cascade on update cascade);
+create table incentives(empno decimal(4,0) references emp(empno) on delete cascade on update cascade, incentive_date date, incentive_amount decimal(10,2), primary key(empno,incentive_date));
+create table project(pno int primary key, pname varchar(30) not null, ploc varchar(30));
+create table assigned_to( empno decimal(4,0) references emp(empno) on delete cascade on update cascade, pno int references project(pno) on delete cascade on update cascade,job_role varchar(30),primary key (empno,pno));
+show tables;
+insert into dept values(10,'ACCOUNTING','MUMBAI'),
+(20,'RESEARCH','BENGALURU'),
+(30,'SALES','DELHI'),
+(40,'OPERATIONS','CHENNAI');
+
+insert into emp values (7369,'adarsh',7902,'2012-12-17','80000.00','20'),
+(7499,'shruthi',7698,'2013-02-20','16000.00','30'),
+(7521,'anvitha',7698,'2015-02-22','12500.00','30'),
+(7566,'tanvir',7839,'2008-04-02','29750.00','20'),
+(7654,'ramesh',7698,'2014-09-28','12500.00','30'),
+(7698,'kumar',7839,'2015-05-01','28500.00','30'),
+(7782,'clark',7839,'2017-06-09','24500.00','10'),
+(7788,'scott',7566,'2010-12-09','30000.00','20'),
+(7839,'king',null,'2009-11-17','50000.00','10'),
+(7844,'turner',7698,'2010-08-17','15000.00','30'),
+(7876,'adams',7788,'2013-12-17','11000.00','20'),
+(7900,'james',7698,'2017-03-17','9500.00','30'),
+(7902,'ford',7566,'2010-03-17','30000.00','20');
+
+select * from emp;
+
+INSERT INTO incentives VALUES(7499,'2019-02-01',5000.00);
+INSERT INTO incentives VALUES(7521,'2019-03-01',2500.00);
+INSERT INTO incentives VALUES(7566,'2022-02-01',5070.00);
+INSERT INTO incentives VALUES(7654,'2020-02-01',2000.00);
+INSERT INTO incentives VALUES(7654,'2022-04-01',879.00);
+INSERT INTO incentives VALUES(7521,'2019-02-01',8000.00);
+INSERT INTO incentives VALUES(7698,'2019-03-01',500.00);
+INSERT INTO incentives VALUES(7698,'2020-03-01',9000.00);
+INSERT INTO incentives VALUES(7698,'2022-04-01',4500.00);
+select * from incentives;
+
+INSERT INTO project VALUES(101,'AI Project','BENGALURU');
+INSERT INTO project VALUES(102,'IOT','HYDERABAD');
+INSERT INTO project VALUES(103,'BLOCKCHAIN','BENGALURU');
+INSERT INTO project VALUES(104,'DATA SCIENCE','MYSURU');
+INSERT INTO project VALUES(105,'AUTONOMUS SYSTEMS','PUNE');
+select * from project;
+
+INSERT INTO assigned_to VALUES(7499,101,'Software Engineer');
+INSERT INTO assigned_to VALUES(7521,101,'Software Architect');
+INSERT INTO assigned_to VALUES(7566,101,'Project Manager');
+INSERT INTO assigned_to VALUES(7654,102,'Sales');
+INSERT INTO assigned_to VALUES(7521,102,'Software Engineer');
+INSERT INTO assigned_to VALUES(7499,102,'Software Engineer');
+INSERT INTO assigned_to VALUES(7654,103,'Cyber Security');
+INSERT INTO assigned_to VALUES(7698,104,'Software Engineer');
+INSERT INTO assigned_to VALUES(7900,105,'Software Engineer');      
+INSERT INTO assigned_to VALUES(7839,104,'General Manager');
+
+select * from assigned_to;
+
+select e.empno
+from emp e, assigned_to a, project p
+where e.empno=a.empno and a.pno=p.pno and
+p.ploc in ('Bengaluru','Hyderabad','Mysuru');
+
+select e.empno,e.ename,d.dname,d.loc,a.job_role,p.ploc
+from emp e, dept d, assigned_to a,project p
+where e.deptno= d.deptno and e.empno=a.empno and a.pno=p.pno and d.loc=p.ploc;
+
+
+
+
+update dept set dname='RESEARCH'
+WHERE deptno=20;
+update dept set dname='sales'
+WHERE deptno=30;
+update dept set dname='OPERATIONS'
+WHERE deptno=40;
+
+
+SELECT empno
+FROM emp
+WHERE empno NOT IN (SELECT empno FROM incentives);
